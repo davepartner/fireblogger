e371964bd55bf231236227a1e87d483359a36a82
daveozoalor <daveozoalor@gmail.com>
2016-09-22 08:55:34 +0100

    updated login code

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { NavController, ModalController, LoadingController, AlertController} from 'ionic-angular';
 import { RegisterPage } from '../register/register';
+import { HomePage } from '../home/home';
+import { UsersService } from '../../providers/users-service/users-service';
+
 /*
   Generated class for the LoginPage page.
 
@@ -9,25 +12,80 @@ import { RegisterPage } from '../register/register';
 */
 @Component({
   templateUrl: 'build/pages/login/login.html',
+  providers:[UsersService]
 })
 export class LoginPage {
 
 public emailField: any; 
 public passwordField: any; 
+private users = [];
+private usersList : any;
 
-
-  constructor(private navCtrl: NavController, private modalCtrl: ModalController) {
+  constructor(private alertCtrl: AlertController , private loadingCtrl: LoadingController, private navCtrl: NavController, private modalCtrl: ModalController, private usersService: UsersService) {
 				
 				this.emailField = "DavePartner@Gmail.com";
+				
+  				this.listOurUsers();
+  
+  }
+  
+  signUserUp(){
+  	
+  	this.usersService.signUpUser(this.emailField, this.passwordField).then(authData => {
+  		//successful
+  		this.navCtrl.setRoot(HomePage);
+  	}, error => {
+  		//alert("error logging in: "+ error.message);
+  		
+  		
+  	});
+  	
+  	
+  	let loader = this.loadingCtrl.create({
+  		dismissOnPageChange: true,
+  	});
+  	
+  	loader.present();
+  	
   }
 
+listOurUsers(){
+	this.usersService.loadUser(10)
+	.then(data => {
+		this.usersList = data;
+	})
+}
 
 
+//login
   submitLogin(){
   	
-  	alert("Hello We just signed in");
+  		
+  	this.usersService.loginUser(this.emailField, this.passwordField).then(authData => {
+  		//successful
+  		this.navCtrl.setRoot(HomePage);
+  	}, error => {
+  		//alert("error logging in: "+ error.message);
+  		let alert = this.alertCtrl.create({
+	      title: 'Error loggin in',
+	      subTitle: error.message,
+	      buttons: ['OK']
+	    });
+	    alert.present();
+  		
+  	});
   	
-  }
+  	
+  	let loader = this.loadingCtrl.create({
+  		dismissOnPageChange: true,
+  	});
+  	
+  	loader.present();
+  	
+  	
+  	
+    }
+ 
   
   submitRegister(){
   	 
@@ -35,4 +93,6 @@ public passwordField: any;
   	 registerModal.present();
   	 
   }
+  
+
 }
---
+++ app/providers/users-service/users-service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import * as firebase from 'firebase';
+/*
+  Generated class for the UsersService provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class UsersService {
+
+private data: any;
+public fireAuth: any;
+public userProfile: any;
+
+
+
+  constructor(private http: Http) {
+  	 
+  	 this.fireAuth = firebase.auth();
+  	 this.userProfile = firebase.database().ref('users');
+  }
+
+loadUser(number){
+	if(this.data){
+		return Promise.resolve(this.data);
+	}
+	
+	return new Promise(resolve => {
+		
+		this.http.get('https://randomuser.me/api/?results='+number)
+			.map(res => res.json())
+			.subscribe(data => {
+				this.data = data.results;
+				resolve(this.data);
+			})
+	})
+}
+
+
+
+
+signUpUser(email: string , password: string){
+	return this.fireAuth.createUserWithEmailAndPassword(email, password).then((newUser) => {
+		//sign in the user
+		this.fireAuth.signInWithEmailAndPassword(email, password).then((authenticatedUser) => {
+			//successful login, create user profile
+		this.userProfile.child(authenticatedUser.uid).set({
+			email: email
+		});	
+		});
+	});
+	
+}
+
+
+loginUser(email: string, password: string): any {
+    return this.fireAuth.signInWithEmailAndPassword(email, password);
+  }
+  
+ logoutUser(){
+ 	return this.fireAUth.signOut();
+ 	
+ 	//redirection
+ }
+
+}
+
